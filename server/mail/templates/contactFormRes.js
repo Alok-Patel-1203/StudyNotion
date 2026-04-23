f32ae230b1324fae58164b40210513769af5b2a1
Alok-Patel-1203 <alokvpatel1203@gmail.com>
2026-04-23 16:39:14 +0530

    Email Sending solved

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -75,7 +75,7 @@ exports.contactUsEmail = (
     <body>
         <div class="container">
             <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                    src="https://res.cloudinary.com/dxiojxog8/image/upload/v1776942460/yh1n8bmmmrwpnnycddmo.png" alt="StudyNotion Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
                 <p>Dear ${firstname} ${lastname},</p>
